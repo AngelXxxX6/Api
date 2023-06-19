2f567b5511200d7869ba40d860c050e03c4d3927
Krapivan <dashchinskiy02@mail.ru>
2023-06-20 00:58:15 +0300

    Doctor Sevice
    and any edits

--- Service/Implementations/DoctorService.cs
+++ Service/Implementations/DoctorService.cs
@@ -7,55 +7,55 @@ using Service.Interfaces;
 
 namespace Service.Implementations
 {
-    public class PatientService : IPatientService
+    public class DoctorService : IDoctorService
     {
-        private readonly IPatientRepository _patientRepository;
-        public PatientService(IPatientRepository patientRepository)
+        private readonly IDoctorRepository _doctorRepository;
+        public DoctorService(IDoctorRepository doctorRepository)
         {
-            _patientRepository = patientRepository;
+            _doctorRepository = doctorRepository;
         }
 
 
-        public async Task<IBaseResponse<IEnumerable<Patient>>> GetPatients()
+        public async Task<IBaseResponse<IEnumerable<Doctor>>> GetDoctors()
         {
-            var baseResponse = new BaseResponse<IEnumerable<Patient>>();
+            var baseResponse = new BaseResponse<IEnumerable<Doctor>>();
             try
             {
-                var patients =  _patientRepository.Select().ToList();
-                if (patients.Count() == 0)
+                var doctors = _doctorRepository.Select().ToList();
+                if (doctors.Count() == 0)
                 {
-                    baseResponse.Description = "Найдено 0 пациентов";
+                    baseResponse.Description = "Найдено 0 докторов";
                     baseResponse.StatusCode = StatusCode.OK;
                     return baseResponse;
                 }
 
-                baseResponse.Data = patients;
+                baseResponse.Data = doctors;
                 baseResponse.StatusCode = StatusCode.OK;
                 return baseResponse;
 
             }
             catch (Exception ex)
             {
-                return new BaseResponse<IEnumerable<Patient>>()
+                return new BaseResponse<IEnumerable<Doctor>>()
                 {
-                    Description = $"[GetPatients] : {ex.Message}"
+                    Description = $"[GetDoctors] : {ex.Message}"
                 };
             }
         }
-
-        public async Task<IBaseResponse<bool>> Create(PatientViewModel patient)
+        public async Task<IBaseResponse<bool>> Create(DoctorViewModel doctor)
         {
             var baseResponse = new BaseResponse<bool>();
             try
             {
-                var Patient = new Patient()
+                var Doctor = new Doctor()
                 {
-                    FIO = patient.FIO,
-                    Adress = patient.Adress,
-                    Phone = patient.Phone,
-                    DateBirthday = patient.DateBirthday,
+                    FIO = doctor.FIO,
+                    Post = doctor.Post,
+                    RoomNumber = doctor.RoomNumber,
+                    WorkTimeStart = doctor.WorkTimeStart,
+                    WorkTimeEnd = doctor.WorkTimeEnd,
                 };
-                await _patientRepository.Create(Patient);
+                await _doctorRepository.Create(Doctor);
             }
             catch (Exception ex)
             {
@@ -66,24 +66,23 @@ namespace Service.Implementations
             }
             return baseResponse;
         }
-
         public async Task<IBaseResponse<bool>> DeleteById(int id)
         {
             var baseResponse = new BaseResponse<bool>();
             try
             {
-                if (await _patientRepository.DeleteById(id))
+                if (await _doctorRepository.DeleteById(id))
                 {
                     baseResponse.Data = true;
                     baseResponse.StatusCode = StatusCode.OK;
                     return baseResponse;
                 }
                 else
-                { 
-                    return new BaseResponse<bool>() 
-                    { 
-                        Description = $"[DeleteById] : {StatusCode.UserNotFound}" 
-                    }; 
+                {
+                    return new BaseResponse<bool>()
+                    {
+                        Description = $"[DeleteById] : {StatusCode.UserNotFound}"
+                    };
                 }
             }
             catch (Exception ex)
@@ -94,25 +93,25 @@ namespace Service.Implementations
                 };
             }
         }
-
-        public async Task<IBaseResponse<bool>> UpdateById(int id, PatientViewModel patient)
+        public async Task<IBaseResponse<bool>> UpdateById(int id, DoctorViewModel doctor)
         {
             var baseResponse = new BaseResponse<bool>();
             try
             {
-                var model = _patientRepository.Select().Where(x => x.Id == id).FirstOrDefault();
-               
+                var model = _doctorRepository.Select().Where(x => x.Id == id).FirstOrDefault();
+
                 if (model == null)
                 {
                     baseResponse.Data = false;
                     baseResponse.StatusCode = StatusCode.UserNotFound;
                     return baseResponse;
                 }
-                model.FIO = patient.FIO;
-                model.Adress = patient.Adress;
-                model.Phone = patient.Phone;
-                model.DateBirthday = patient.DateBirthday;
-                if (await _patientRepository.Update(model))
+                model.FIO = doctor.FIO;
+                model.Post = doctor.Post;
+                model.RoomNumber = doctor.RoomNumber;
+                model.WorkTimeStart = doctor.WorkTimeStart;
+                model.WorkTimeEnd = doctor.WorkTimeEnd;
+                if (await _doctorRepository.Update(model))
                 {
                     baseResponse.StatusCode = StatusCode.OK;
                     baseResponse.Data = true;
---
+++ Service/Interfaces/IDoctorService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Enitity;
+using Domain.Response;
+
+namespace Service.Interfaces
+{
+    public interface IDoctorService
+    {
+        Task<IBaseResponse<IEnumerable<Doctor>>> GetDoctors();
+        Task<IBaseResponse<bool>> Create(DoctorViewModel doctor);
+        Task<IBaseResponse<bool>> DeleteById(int id);
+        Task<IBaseResponse<bool>> UpdateById(int id, DoctorViewModel doctor);
+    }
+}
